a773612df817b676c176259c8208a04f753be400
Si-ID <noval27nov@gmail.com>
2025-09-03 21:02:31 +0700

    Add blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,15 @@
+// import Navbar from "../../components/Navbar";
+
+export default function Blog() {
+  return (
+    <main>
+      {/* <Navbar /> */}
+      <h1>Blog</h1>
+      <ul>
+        <li>Post 1: Getting started with Next.js</li>
+        <li>Post 2: Collaboration workflow in GitHub</li>
+        <li>Post 3: Why teamwork matters</li>
+      </ul>
+    </main>
+  );
+}
