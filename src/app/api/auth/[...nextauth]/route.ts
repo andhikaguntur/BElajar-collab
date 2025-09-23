b82478495d72c486ba594faa6ba766f2587fdb51
Si-ID <noval27nov@gmail.com>
2025-09-23 22:36:20 +0700

    api-spotify-ez-bgt

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,11 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaClient } from "@prisma/client";
+import SpotifyProvider from "next-auth/providers/spotify";
 
 const prisma = new PrismaClient();
+
+
 
 export const authOptions = {
   providers: [
@@ -31,6 +34,30 @@ export const authOptions = {
   },
   secret: process.env.NEXTAUTH_SECRET,
 };
+
+export default NextAuth({
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_CLIENT_ID!,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
+      authorization:
+        "https://accounts.spotify.com/authorize?scope=user-read-email,user-top-read,playlist-read-private",
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      (session as any).accessToken = token.accessToken;
+      return session;
+},
+  },
+});
 
 const handler = NextAuth(authOptions);
 
